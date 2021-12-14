4d7890059219315483265fba4509e2b06ba4ce4a
Diegofergamboa <diegofergamboa@hotmail.es>
2021-12-14 15:21:29 -0500

    Create index.js

---
+++ src/es6/index.js
@@ -0,0 +1,48 @@
+
+function newFunction(name, age, country) {
+    var name = name || 'oscar' ;
+    var name = age || 25 ;
+    var name = country || 'country' ;
+    console.log(name, age, country);
+}
+
+// es6 
+
+function newFunction2(name = 'scar' , age = 32 , country="MX") {
+    console.log(name, age, country)
+}
+newFunction2();
+newFunction2('diego', 1, 'colombia');
+
+// template literal
+
+let hello = 'hello';
+let world = 'world';
+
+let epicPhrase = `${hello} ${world} hola como vas`;
+console.log(epicPhrase);
+
+//multilinea en los strings
+
+let lorem = "lorem ipsum qui consequatur commodi ipsum vel duis PRIMERA FRASE \n"
++ "frase de OTRA LINEA";
+console.log(lorem);
+
+
+let lorem2 = `PRIMERA LINEA
+SEGUNDA LINEA`;
+console.log(lorem2);
+
+// destructuring 
+
+let person = {
+    'name' : 'diego',
+    'age': 25 ,
+    'country' : 'colombia'
+}
+console.log(person.name);
+console.log(person.age);
+console.log(person.country);
+
+let {name, age, country} = person
+console.log(name, age, country);
