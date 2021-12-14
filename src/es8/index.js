61811d4006e50890bdc2817f1c7271cfeb9530a0
Diegofergamboa <diegofergamboa@hotmail.es>
2021-12-14 16:54:15 -0500

    Update index.js

--- src/es8/index.js
+++ src/es8/index.js
@@ -9,3 +9,16 @@ const data = {
 const entries = Object.entries(data);
 
 console.log(entries);
+console.log(entries.length)
+
+const data = {
+    frontend: 'Oscar' ,
+    backend: 'Isabel',
+    designer: 'Ana'
+}
+
+const values = Object.values(data);
+console.log(values.length);
+
+const string = 'hello';
+console.log(string.padStart(8, 'hi '))
