0aa97c15c922a22397db10e057590fa4b0c6a602
Diegofergamboa <diegofergamboa@hotmail.es>
2021-12-14 18:28:52 -0500

    aaa

---
+++ src/es6/es11/index.js
@@ -0,0 +1,10 @@
+// ES 11
+
+//dinamic import, hacer importación de forma dinámica par apoder llamar piezas de codigo para optimizar el proyecto
+
+const button = document.getElementById('btn');
+
+button.addEventListener('click' , async function () {
+    const module = await import("./file.js");
+    module.hello();
+});
