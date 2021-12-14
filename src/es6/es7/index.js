49fcdb285b81643c15377e342e79d1b87fa8dc6e
Diegofergamboa <diegofergamboa@hotmail.es>
2021-12-14 16:43:29 -0500

    entries para ECMA8

---
+++ src/es6/es7/index.js
@@ -0,0 +1,18 @@
+// validación
+
+let numbers = [1,2,3,4,5,6,7,8,4,3,2,5,6,7,8,9,1];
+
+if (numbers.includes(100)) {
+    console.log('Si está el valor 7.')
+} else {
+    console.log('No se encuentra el valor de 100.')
+}
+
+// Elevar a la potencia.
+
+let base = 4;
+let exponente = 3;
+
+let result = base ** exponente;
+
+console.log(result);
---
+++ src/es8/index.js
@@ -0,0 +1,11 @@
+// devolver clave y valor de una matriz
+
+const data = {
+    frontend: 'Oscar' ,
+    backend: 'Isabel',
+    designer: 'Ana'
+}
+
+const entries = Object.entries(data);
+
+console.log(entries);
